6c687cae602821db2453ba622ecdc76cb800008b
dwparker79 <dwparker79@gmail.com>
2017-02-14 07:10:35 -0500

    Add files via upload

---
+++ NAPSTaskExecutorTest/TaskExecutorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAPSTaskExecutor
+{
+    class TaskExecutorTest
+    {
+        public static void Main(String[] args)
+        {
+            TaskExecutor te = TaskExecutor.NewTask("C:\\tmp\\echoadsfsadfsadf.bat", new[] {"hello,", "Greg! ", "This", "is", "a", "test"});
+            bool foo;
+            Console.WriteLine(te.Execute(out foo));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NAPSTaskManagerTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NAPSTaskExecutor;
+using NAPSTaskManager;
+
+namespace NAPSTaskManagerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating tasks...");
+            TaskManager tm = new TaskManager();
+            for (int i = 0; i < 200; i += 5)
+            {
+                tm.AddTask("C:\\tmp\\echo.bat", "C:\\tmp\\echo\\echo" + i.ToString("000") + ".log", "oiasdjfoiajsdfoijasdf", "sdlkjf", "alsdkjf", "alskdjf");
+                tm.AddTask("C:\\tmp\\echo.bat", "C:\\tmp\\echo\\echo" + (i + 1).ToString("000") + ".log", "Hello,", "world!");
+                tm.AddTask("C:\\tmp\\echo.bat", "C:\\tmp\\echo\\echo" + (i + 2).ToString("000") + ".log", "oiasdjfoiajsdfoijasdf", "sdlkjf", "alsdkjf", "alskdjf");
+                tm.AddTask("C:\\tmp\\echo.bat", "C:\\tmp\\echo\\echo" + (i + 3).ToString("000") + ".log", "w");
+                tm.AddTask("C:\\tmp\\echo.bat", "C:\\tmp\\echo\\echo" + (i + 4).ToString("000") + ".log", "oiasdjfoiajsdfoijasdf", "sdlkjf", "alsdkjf", "alskdjf");
+            }
+            Console.WriteLine("Tasks Created.  Waiting for TaskManager to finish...");
+            DateTime waitTime = DateTime.Now.AddSeconds(1);
+            while (tm.CurrentlyWorking)
+            {
+                if (DateTime.Now > waitTime)
+                {
+                    waitTime = waitTime.AddSeconds(1);
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine(" done.");
+            Console.WriteLine(File.Exists("C:\\tmp\\echo\\echo001.log"));
+            Console.ReadKey();
+        }
+    }
+}
